d6fdf1fb565a3d0229246579ab81e0287660394a
MJArthaRamandika <ramandikajovanka@gmail.com>
2020-05-01 09:49:51 +0700

    persiapanModul

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -4,32 +4,44 @@ import Post from "../../component/BlogPost/Post";
 import API from "../../services";
 
 class BlogPost extends Component{
-    state = {                    // komponen state dari React untuk statefull component
-        listArtikel: [],         // variabel array yang digunakan untuk menyimpan data API
-        insertArtikel: {         // variable yang digunakan untuk menampung sementara data yang akan di insert
-            userId: 1,           // kolom userId, id, title, dan body sama, mengikuti kolom yang ada pada listArtikel.json
-            id: 1,
-            title: "",
-            body: ""
+    constructor(props){
+        super(props);
+        firebaseConfig.initializeApp(firebaseConfig);
+
+        this.state = {
+            listArtikel: []
         }
     }
     ambilDataDariServerAPI = () => {                // fungsi untuk mengambil data dari API dengan penambahan sort dan order
-        API.getNewsBlog().then(result => {
-            this.setState({
-                listArtikel: result
-            })
-        })
+        let ref = firebase.database().ref("/");
+        ref.on("value", snapshot => {
+            const state = snapshot.val();
+            this.setState(state);
+        });
+    }
+
+    simpanDataKeServerAPI = () => {
+        firebase.database()
+            .ref("/")
+            .set(this.state);
     }
 
     componentDidMount() {       // komponen untuk mengecek ketika compnent telah di-mount-ing, maka panggil API
         this.ambilDataDariServerAPI()  // ambil data dari server API lokal
     }
 
+    componentDidUpdate(prevProps, prevState){
+        if (prevState !== this.state){
+            this.simpanDataKeServerAPI();
+        }
+    }
+
     handleHapusArtikel = (data) => {        // fungsi yang meng-handle button action hapus data
-        API.deleteNewsBlog(data)
-            .then(res => {      // ketika proses hapus berhasil, maka ambil data dari server API lokal
-                this.ambilDataDariServerAPI()
-            })
+        const{listArtikel} = this.state;
+        const newState =listArtikel.filter(data => {
+            return data.uid !== idArtikel;
+        });
+        this.setState({listArtikel: newState})
     }
 
     handleTambahArtikel = (event) => {      // fungsi untuk meng-hadle form tambah data artikel
@@ -43,10 +55,28 @@ class BlogPost extends Component{
     }
 
     handleTombolSimpan = () => {            // fungsi untuk meng-handle tombol simpan
-        API.postNewsBlog(this.state.insertArtikel)
-            .then( (response) => {
-                this.ambilDataDariServerAPI();                  // reload / refresh data
+        let title = this.refs.judulartikel.value;
+        let body = this.refs.isiartikel.value;
+        let uid = this.refs.uid.value;
+
+        if (uid && title && body){
+            const {listArtikel} = this.state;
+            const indeksArtikel = listArtikel.findIndex(data => {
+                return data.uid === uid;
             });
+            listArtikel[indeksArtikel].title = title;
+            listArtikel[indeksArtikel].body = body;
+            this.state({listArtikel});
+        } else if (title && body){
+            const uid = new Date().getTime().toString();
+            const{ listArtikel } = this.state;
+            listArtikel.push({uid, title, body});
+            this.setState({listArtikel});
+        }
+
+        this.refs.judulartikel.value = "";
+        this.refs.isiartikel.value = "";
+        this.refs.uid.value = "";
     }
 
     render() {
@@ -56,21 +86,23 @@ class BlogPost extends Component{
                     <div className="form-group row">
                         <label htmlFor="title" className="col-sm-2 col-form-label">Judul</label>
                         <div className="col-sm-10">
-                            <input type="text" className="form-control" id="title" name="title" onChange={this.handleTambahArtikel}/>
+                            <input type="text" className="form-control" id="title" name="title" ref="judulArtikel" />
                         </div>
                     </div>
                     <div className="form-group row">
                         <label htmlFor="body" className="col-sm-2 col-form-label">Isi</label>
                         <div className="col-sm-10">
-                            <textarea className="form-control" id="body" name="body" rows="3" onChange={this.handleTambahArtikel}></textarea>
+                            <textarea className="form-control" id="body" name="body" rows="3" ref="isiArtikel"></textarea>
                         </div>
                     </div>
+                    <input type="hidden" name="uid" ref="uid"/>
                     <button type="submit" className="btn btn-primary" onClick={this.handleTombolSimpan}>Simpan</button>
                 </div>
                 <h2>Daftar Artikel</h2>
                 {
                     this.state.listArtikel.map(artikel => {  // looping dan masukkan untuk setiap data yang ada di listArtikel ke variabel artikel
-                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/>     // mappingkan data json dari API sesuai dengan kategorinya
+                        return <Post key={artikel.id} judul={artikel.title} isi={artikel.body} 
+                                    idArtikel={artikel.id} hapusArtikel={this.handleHapusArtikel}/>     // mappingkan data json dari API sesuai dengan kategorinya
                     })
                 }
             </div>
---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-bXkYIPIKOTZfm8yB04UFvM4ntJJRHLg",
+    authDomain: "fir-login-23028.firebaseapp.com",
+    databaseURL: "https://fir-login-23028.firebaseio.com",
+    projectId: "fir-login-23028",
+    storageBucket: "fir-login-23028.appspot.com",
+    messagingSenderId: "676482831417",
+    appId: "1:676482831417:web:0e30b4d511eac28312e9e8",
+    measurementId: "G-7Y044M8XSP"
+  };
